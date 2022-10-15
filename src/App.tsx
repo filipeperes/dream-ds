c46bd6a83d5cec7f4ba2bfbd4a3983ae2fe017de
Filipe Peres <contato@filipeperes.com>
2022-10-15 15:55:07 -0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+export function App() {
+ 
+  return (
+    <>
+    <h1 className='text-2xl font-bold'>Hello World</h1>
+    <button className='bg-cyan-500 fonte-medium px-4 py-4 text-white hover:bg-cyan-500'>
+      Enviar
+    </button>
+    </>
+  )
+}
---
+++ storybook-static/assets/preview.1bc245ac.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.5b922925.js";import"./iframe.6b20a49c.js";import"./es.map.constructor.5aa0c0a7.js";import"./es.number.to-fixed.d480cb90.js";import"./index.60f73bd5.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.1bc245ac.js.map
